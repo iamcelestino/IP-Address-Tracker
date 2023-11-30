b3a7f6a8dd2882038e227018a182faadad5039d4
iamcelestino <trcelestino488@gmail.com>
2023-11-30 07:41:56 -0800

    added the logic to get that

--- src/calculus-ui/logic.js
+++ src/calculus-ui/logic.js
@@ -1,10 +1,20 @@
 
 import "./style/style.css";
 
-class Calculus {
+class IPaddress {
      constructor() {
+       this.IPaddressUrl = 'https://geo.ipify.org/api/v2/country?apiKey=at_Kp31QZqTgfJtLVeR8MGd64Em5ZDaq&';  
+     }
+     async getIpAddress(ip) {
+          const query = `ipAddress=${ip}`;
+          const response = await fetch(this.IPaddressUrl + query);
+          const data = await response.json();
+          return data;
+     }
+     UpadateUi () {
+          
      }
 }
 
-export {Calculus as default};
+export {IPaddress as default};
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import Calculus from "./calculus-ui/logic";
+import IPaddress  from "./calculus-ui/logic";
+const ipAddress = new IPaddress();
+const form = document.querySelector('form');
+
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const ip = form.address.value;
+    ipAddress.getIpAddress(ip)
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+});
+
+
+
 
 
 
