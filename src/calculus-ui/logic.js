7259bf06d2428250576c7a824f8525504f8e16e4
iamcelestino <trcelestino488@gmail.com>
2023-12-02 08:29:41 -0800

    render all ip information on the html page

--- src/calculus-ui/logic.js
+++ src/calculus-ui/logic.js
@@ -1,24 +1,44 @@
-
 import "./style/style.css";
 
 class IPaddress {
+
      constructor(container) {
           this.container = container;
           this.IPaddressUrl = 'https://geo.ipify.org/api/v2/country?apiKey=at_Kp31QZqTgfJtLVeR8MGd64Em5ZDaq&';  
      }
+
      async getIpAddress(ip) {
           const query = `ipAddress=${ip}`;
           const response = await fetch(this.IPaddressUrl + query);
           const data = await response.json();
           return data;
      }
+
      UpadateUi (data) {
-          const address = document.createElement('p');
+
+          const paragraph = [];
+          for (let i = 0; i < 4; i++) {
+               const p = document.createElement('p'); 
+               paragraph.push(p);  
+          }
+
+          const ip = paragraph[0];
+          ip.textContent = data.ip;
+          this.container.childNodes[1].append(ip);
           
-          address.textContent = data.ip;
-          this.container.childNodes[1].append(address);
-          console.log(data.ip, data.location.country, data.isp,  data.location.timezone);
+          const location = paragraph[1];
+          location.textContent = data.location.country;
+          this.container.childNodes[3].append(location);
+          
+          const timezone = paragraph[2];
+          timezone.textContent  =  data.location.timezone;
+          this.container.childNodes[3].append(timezone);
+
+          const isp = paragraph[3];
+          isp.textContent = data.isp;
+          this.container.childNodes[7].append(isp);
      }
+
 }
 
 export {IPaddress as default};
